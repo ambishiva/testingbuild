190099fab1973874ed624071df36670b77e16a46
shiva <shivasinghal123@gmail.com>
2021-06-30 10:59:16 +0530

    Added secret code

--- app/src/main/java/com/test/testingbuild/MainActivity.kt
+++ app/src/main/java/com/test/testingbuild/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.test.testingbuild
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "1b7d5ec8-6c4a-4af7-8a2b-204939ace628",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
